c68d6f9e2e560aa32c49865f2399138e8ef31cf4
ritesh530 <riteshsalunkhe530@gmail.com>
2022-06-20 11:58:39 +0530

    Final commit : completed with account page .

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,23 +1,30 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import ProtectedRoute from "./Components/ProtectedRoute";
 import { AuthContextProvider } from "./Context/AuthContext";
 import Account from "./Pages/Account";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 
-
 function App() {
   return (
     <>
       <AuthContextProvider>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/account" element={<Account/>}/>
-      </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
       </AuthContextProvider>
     </>
   );
